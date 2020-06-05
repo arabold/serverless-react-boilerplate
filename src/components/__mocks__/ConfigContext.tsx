6a206e2c89b44f55034ccf16380864294a936eeb
Andre Rabold <andre.rabold@bcgdv.com>
2020-06-05 08:13:28 -0700

    Use lowercase `manifest` variable name for consistency

--- src/components/__mocks__/ConfigContext.tsx
+++ src/components/__mocks__/ConfigContext.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-import Manifest from "../../../public/manifest.json";
+import manifest from "../../../public/manifest.json";
 import { Config } from "../../server/config";
 
 const ConfigContext = React.createContext<Config>({
   app: {
-    TITLE: `${Manifest.short_name} Mock`,
-    THEME_COLOR: Manifest.theme_color,
+    TITLE: `${manifest.short_name} Mock`,
+    THEME_COLOR: manifest.theme_color,
     URL: "http://localhost:3000",
     DIST_URL: "http://localhost:8080",
     PUBLIC_URL: "http://localhost:8080",
