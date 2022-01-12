f1ba47596bc814c2b848fdd65ae31ae8c7cc0377
Andre Rabold <andre.rabold@gmail.com>
2022-01-12 06:41:26 -0800

    Fixed path to App.tsx in intro text

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ export default function App() {
         <h1 className="App-title">Welcome to {config.app.TITLE}</h1>
       </header>
       <p className="App-intro">
-        To get started, edit <code>src/browser/App.jsx</code> and save to reload.
+        To get started, edit <code>src/App.tsx</code> and save to reload.
       </p>
     </div>
   );
